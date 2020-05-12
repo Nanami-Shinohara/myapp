372398380b8a548b2339eab1357f08bb3beeb882
Nanami-Shinohara <63773033+Nanami-Shinohara@users.noreply.github.com>
2020-05-12 15:32:32 +0900

    Add files via upload
    error2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import TaskBoard from './taskBoard';
+// import TaskBoard from './TaskBoard';
 import * as serviceWorker from './serviceWorker';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from "history";
